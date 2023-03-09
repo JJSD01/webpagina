adbbe0c6f9bc2e119799a7cb92ed15abf764449b
jonathan <jonathan.sanchez.8005@miremington.edu.co>
2023-03-09 00:04:12 -0500

    agregando datos al body

--- jfk-instrumentos-r/src/componentes/Body/Body.js
+++ jfk-instrumentos-r/src/componentes/Body/Body.js
@@ -1,5 +1,5 @@
+import fender from '../../IMG/fender.png';
 import React, { Component } from 'react';
-import fender from './IMG/fender.png';
 import './Body.css';
 
 
@@ -19,8 +19,9 @@ class Body extends Component {
                                     <h2>Fender</h2>
                                     <p>Una opción perfecta para los principiantes, el nuevo Strat® Pack de Squier® tiene todo lo necesario para empezar a tocar, la Stratocaster de escala corta (24″) es ideal para músicos con manos más pequeñas y proporciona una sensación más cómoda. Otras características incluyen un cuerpo ligero de peso, un puente fijo para una afinación más estable y tres pastillas de una sola bobina para el tono Strat clásico.</p>
                                     <div className='imagen1'>
-                                        <img src={fender} alt="" />
+                                        <img src={fender} alt=""/>
                                     </div>
+                                    
 
 
 
@@ -34,7 +35,29 @@ class Body extends Component {
 
 
                     </div>
+                    <div className='contenido'>
+                        <div className='articulos'>
+                            <div className='secciones'>
+                                <div>
+                                    <h2>Fender</h2>
+                                    <p>Una opción perfecta para los principiantes, el nuevo Strat® Pack de Squier® tiene todo lo necesario para empezar a tocar, la Stratocaster de escala corta (24″) es ideal para músicos con manos más pequeñas y proporciona una sensación más cómoda. Otras características incluyen un cuerpo ligero de peso, un puente fijo para una afinación más estable y tres pastillas de una sola bobina para el tono Strat clásico.</p>
+                                    <div className='imagen1'>
+                                        <img src={fender} alt=""/>
+                                    </div>
+                                    
 
+
+
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
 
 
 
