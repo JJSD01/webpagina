3136a8630705acb95c865497a6276f87c958fe04
Frank Martinez <frank.martinez.0569@uniremington.edu.co>
2023-03-14 21:39:02 -0500

    terminando lista de productos y carrito

--- jfk-instrumentos-r/src/componentes/Listadeproductos/Productos.js
+++ jfk-instrumentos-r/src/componentes/Listadeproductos/Productos.js
@@ -1,39 +1,52 @@
 import React from 'react';
-import a from '../../IMG/a.jpg';
+import g1 from '../../IMG/g1.jpg';
+import maracas from '../../IMG/maracas.jpg';
+import flauta2 from '../../IMG/flauta2.jpg';
+import piano3 from '../../IMG/piano3.jpg';
+import baj from '../../IMG/baj.png';
+import guit from '../../IMG/guit.png';
+import orga from '../../IMG/orga.png';
+import timba from '../../IMG/timba.png';
+
 
 const productos = [
   {
     nombre: 'Producto 1',
-    imagen: a,
+    imagen:  g1,
     precio: '$10'
   },
   {
     nombre: 'Producto 2',
-    imagen: 'https://via.placeholder.com/150',
+    imagen:  maracas,
     precio: '$15'
   },
   {
     nombre: 'Producto 3',
-    imagen: 'https://via.placeholder.com/150',
+    imagen: flauta2,
     precio: '$20'
   },
   {
-    nombre: 'Producto 2',
-    imagen: 'https://via.placeholder.com/150',
+    nombre: 'Producto 4',
+    imagen:  piano3,
     precio: '$15'
   },
   {
-    nombre: 'Producto 2',
-    imagen: 'https://via.placeholder.com/150',
+    nombre: 'Producto 5',
+    imagen:  baj,
     precio: '$15'
   },{
-    nombre: 'Producto 2',
-    imagen: 'https://via.placeholder.com/150',
+    nombre: 'Producto 6',
+    imagen:  guit,
     precio: '$15'
   },
   {
-    nombre: 'Producto 2',
-    imagen: 'https://via.placeholder.com/150',
+    nombre: 'Producto 7',
+    imagen: orga,
+    precio: '$15'
+  },
+  {
+    nombre: 'Producto 8',
+    imagen:  timba,
     precio: '$15'
   },
   
@@ -41,10 +54,10 @@ const productos = [
 
 const Productos = () => {
   return (
-    <div>
+    <div  className="productos-container">
       <h1>Lista de productos</h1>
       {productos.map(producto => (
-        <div key={producto.nombre}>
+        <div key={producto.nombre} className="producto">
           <img src={producto.imagen} alt={producto.nombre} />
           <p>{producto.nombre} - {producto.precio}</p>
         </div>
